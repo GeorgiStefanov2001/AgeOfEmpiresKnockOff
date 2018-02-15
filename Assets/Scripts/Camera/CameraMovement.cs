d3ea4772ffe4e6c8839e532d8961d626768cbb9d
George <32243181+GeorgiStefanov2001@users.noreply.github.com>
2018-02-15 21:07:07 +0200

    Updated
    Rewored the camera movement

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -14,34 +14,42 @@ public class CameraMovement : MonoBehaviour {
 		
 	}
 
-	void Update () {
+    void Update()
+    {
+        PlayerPrefs.SetInt("MouseMovement", 1); //for testing purposes
+        if (PlayerPrefs.GetInt("MouseMovement") == 1) //checking the player's desired camera movement method
+        {
+            MouseMovement();
+        }
 
-        //getting the X and Y position of the mouse
-        float mouseX = Input.mousePosition.x;
-        float mouseY = Input.mousePosition.y;
+    }
+    void MouseMovement(){
+            //getting the X and Y position of the mouse
+            float mouseX = Input.mousePosition.x;
+            float mouseY = Input.mousePosition.y;
 
-        //if the mouse is close to the right edge of the screen we move it to the right
-        if (mouseX >= (Screen.width - distance))
-        {
-            transform.Translate(Vector3.right*speed*Time.deltaTime);
-        }
-        //if the mouse is close to the left edge of the screen we move it to the left
-        else if (mouseX <= distance)
-        {
-            transform.Translate(Vector3.left * speed* Time.deltaTime); 
-        }
-        //if the mouse is close to the top edge of the screen we move it up
-        if (mouseY >= (Screen.height - distance))
-        {
-            transform.Translate(Vector3.forward * speed* Time.deltaTime);
-            transform.Translate(Vector3.up * speed * Time.deltaTime); //the camera is at a 45 degree angle so moving it forward will cause it to go to the ground 
-                                                                      //so we move it up at the same time to balance it and make it go straight
-        }
-        //if the mouse is close to the bottom edge of the screen we move it down
-        else if (mouseY <= distance)
-        {
-            transform.Translate(Vector3.back * speed* Time.deltaTime); 
-            transform.Translate(Vector3.down * speed * Time.deltaTime); //same here but this time we pull it down
-        }
+            //if the mouse is close to the right edge of the screen we move it to the right
+            if (mouseX >= (Screen.width - distance))
+            {
+                transform.Translate(Vector3.right*speed*Time.deltaTime);
+            }
+            //if the mouse is close to the left edge of the screen we move it to the left
+            else if (mouseX <= distance)
+            {
+                transform.Translate(Vector3.left * speed* Time.deltaTime); 
+            }
+            //if the mouse is close to the top edge of the screen we move it up
+            if (mouseY >= (Screen.height - distance))
+            {
+                transform.Translate(Vector3.forward * speed* Time.deltaTime);
+                transform.Translate(Vector3.up * speed * Time.deltaTime); //the camera is at a 45 degree angle so moving it forward will cause it to go to the ground 
+                                                                          //so we move it up at the same time to balance it and make it go straight
+            }
+            //if the mouse is close to the bottom edge of the screen we move it down
+            else if (mouseY <= distance)
+            {
+                transform.Translate(Vector3.back * speed* Time.deltaTime); 
+                transform.Translate(Vector3.down * speed * Time.deltaTime); //same here but this time we pull it down
+            }
     }
 }
