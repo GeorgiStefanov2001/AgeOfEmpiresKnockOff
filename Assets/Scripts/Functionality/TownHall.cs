ac49f45135cbf6eecb10ed7be3429a8d7bec0844
VladislavM <35681810+VladislavMilenkov@users.noreply.github.com>
2018-02-16 22:44:21 +0200

    Health
    added healt bar and end game savings

--- Assets/Scripts/Functionality/TownHall.cs
+++ Assets/Scripts/Functionality/TownHall.cs
@@ -5,17 +5,29 @@ using UnityEngine.UI;
 
 public class TownHall : MonoBehaviour
 {
-    public int level = 0;
+    public int level;
     bool canLevelUp = false;
     float colliderRange = 100f;
     bool showStats = false;
+    public int maxHp = 0;
+    public int currHealth = 0;
+    public int population = 0;
+    public int numOfHouses = 0;
+    public string leader;
 
     GameObject townHallInfoPanel;
+    public GameObject HP;
 
     void Start()
     {
-        level = level == 0 ? 1 : PlayerPrefs.GetInt("TownHallLvl"); //if the level is 0 (new game) then we set it to 1, otherwise we set it to the players level that is saved on the hard
-        townHallInfoPanel = GameObject.Find("TownHallPanel"); //we get the panel for the town hall info
+        population =PlayerPrefs.GetInt("Population");
+        numOfHouses = PlayerPrefs.GetInt("NumberOfHouses"); 
+        maxHp = PlayerPrefs.GetInt("maxHealth"); 
+        currHealth = PlayerPrefs.GetInt("CurrentHealth");
+        level = PlayerPrefs.GetInt("TownHallLvl"); 
+        townHallInfoPanel = GameObject.Find("TownHallPanel");  
+        leader = PlayerPrefs.GetString("Leader"); 
+
     }
 
     void Update()
@@ -23,6 +35,7 @@ public class TownHall : MonoBehaviour
         GetComponent<BoxCollider>().size = new Vector3((transform.localScale.x - 0.1f) * colliderRange, (transform.localScale.y - 0.1f) * colliderRange * 2, (transform.localScale.z - 0.1f) * colliderRange); //setting the size of the town hall's collider
         LevelUp();
         CheckStats();
+        AddjustHealth(0);
     }
 
     void LevelUp()
@@ -51,5 +64,36 @@ public class TownHall : MonoBehaviour
     public void CloseStats()
     {
         showStats = false;
+    }
+
+    public void AddjustHealth (int addj) { // in case we take damage or heal
+
+        currHealth += addj;
+
+        if (currHealth <= 0)
+        {
+            currHealth = 0;
+        }else if(currHealth > maxHp)
+        {
+            currHealth = maxHp;
+        }
+
+        Health();
+    } 
+
+    public void Health()
+    {
+        HP.GetComponent<Text>().text = "Health: " + currHealth + " / " + maxHp;
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("TownHallLvl", level);
+        PlayerPrefs.SetInt("maxHealth", maxHp);
+        PlayerPrefs.SetInt("CurrentHealth", currHealth);
+        PlayerPrefs.SetInt("Population", population);
+        PlayerPrefs.SetInt("NumberOfHouses", numOfHouses);
+        PlayerPrefs.SetString("Leader", leader);
     }
 }
